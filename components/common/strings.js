db5e3ca679e1aef892f42d90b522e83b960b0e10
4stern <git@4stern.net>
2014-04-17 00:58:10 +0200

    adding better comment to explain the function logic

--- components/common/strings.js
+++ components/common/strings.js
@@ -2,6 +2,6 @@
  *  Common strings component
  */
 exports.urlPrepare = function(string) {
-    // Prepare URL string
+    // Prepare URL string for/to nothing else as the string itself
     return string;
 }
